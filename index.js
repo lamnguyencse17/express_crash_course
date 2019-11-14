e4b2111e60dc87e1faa81e668c699d15d7e77620
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2019-11-14 10:03:03 +0700

    Initialized ExpressJS
    Serving static folder
    Basic but hardcoded REST API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+
+const app = express();
+
+// Gets All Members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
