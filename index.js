2e15f16a35f5bfe3669ca79873e26c255c960596
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2019-11-14 21:46:00 +0700

    Simple API Query

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
 const members = require('./Members');
+const logger = require('./middleware/logger');
 
 const app = express();
+
+//Init middleware
+// app.use(logger)
 
 // Gets All Members
 app.get('/api/members', (req, res) => {
     res.json(members);
+});
+
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id ===  parseInt(req.params.id));
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }
+    else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+    }
 });
 
 // Set a static folder
